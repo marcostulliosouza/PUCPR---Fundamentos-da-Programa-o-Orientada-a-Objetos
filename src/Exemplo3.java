08631e39972340bd35ce0393470ff1a61e03f637
Marcos Souza <marcostullio.s@gmail.com>
2024-06-07 07:23:17 -0300

    terceiro exemplo concluído

---
+++ src/Exemplo3.java
@@ -0,0 +1,14 @@
+/*3. Crie um algoritmo em Java que mostre na tela todos os
+múltiplos de 3 entre os números 0 e 30 usando a estrutura “do while”.*/
+public class Exemplo3 {
+    public static void main(String[] args){
+        int numero = 0;
+
+        do{
+            if (numero % 3 == 0){
+                System.out.println(numero);
+            }
+            numero += 1;
+        }while (numero <= 30);
+    }
+}
