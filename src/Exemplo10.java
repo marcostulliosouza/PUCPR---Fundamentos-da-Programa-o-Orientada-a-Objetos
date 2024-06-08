a120e1073bf849538b742a5a970bd9e2e7399037
Marcos Souza <marcostullio.s@gmail.com>
2024-06-08 17:40:01 -0300

    decimo exemplo

--- src/Exemplo10.java
+++ src/Exemplo10.java
@@ -1,33 +1,64 @@
-/*10. Crie um algoritmo em Java contendo a lógica do jogo da forca.
-O programa deve escolher aleatoriamente uma palavra de uma lista de palavras
-e o usuário deve adivinhar a palavra, uma letra de cada vez. O usuário tem
-um número fixo de tentativas para adivinhar a palavra.*/
-
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+
 public class Exemplo10 {
-    public static void main(String[] args){
-        List<String> minhaLista = new ArrayList<>();
-        Random aleatorio = new Random();
+    public static void main(String[] args) {
+        String[] palavras = {"carro", "uva", "alegria", "strogonoff"};
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        String palavraSelecionada = palavras[random.nextInt(palavras.length)];
+        int tentativas = 6;
+        StringBuilder palavraAdivinhada = new StringBuilder();
 
-        minhaLista.add("casa");
-        minhaLista.add("carro");
-        minhaLista.add("banana");
-        minhaLista.add("alegria");
-
-        int indiceAleatorio = aleatorio.nextInt(minhaLista.size());
-        String elementoAleatorio = minhaLista.get(indiceAleatorio);
-        StringBuilder mensagem = new StringBuilder();
-        boolean aux = false;
-
-        String espacos = "";
-        for (int i = 1; i < elementoAleatorio.length(); i++){
-            mensagem.append(espacos = espacos + " _ ");
+        for (int i = 0; i < palavraSelecionada.length(); i++) {
+            palavraAdivinhada.append('_');
         }
-        System.out.println(mensagem);
-        System.out.println(elementoAleatorio.length() + " " + elementoAleatorio);
+
+        Set<Character> letrasAdivinhadas = new HashSet<>();
+
+        System.out.println("Bem-vindo ao jogo da Forca!");
+        System.out.println("Tente adivinhar a palavra!");
+
+        while (tentativas > 0) {
+            System.out.println("Palavra: " + palavraAdivinhada);
+            System.out.println("Tentativas restantes: " + tentativas);
+            System.out.print("Digite uma letra: ");
+
+            char letra = scanner.nextLine().toLowerCase().charAt(0);
+
+            if (letrasAdivinhadas.contains(letra)) {
+                System.out.println("Você já tentou essa letra. Tente outra.");
+                continue;
+            }
+
+            letrasAdivinhadas.add(letra);
+            boolean letraAdivinhada = false;
+
+            for (int i = 0; i < palavraSelecionada.length(); i++) {
+                if (palavraSelecionada.charAt(i) == letra) {
+                    palavraAdivinhada.setCharAt(i, letra);
+                    letraAdivinhada = true;
+                }
+            }
+
+            if (!letraAdivinhada) {
+                tentativas--;
+            }
+
+            if (palavraAdivinhada.toString().equals(palavraSelecionada)) {
+                System.out.println("Parabéns, você adivinhou a palavra correta: " + palavraSelecionada);
+                break;
+            }
+        }
+
+        if (tentativas == 0) {
+            System.out.println("Game Over! A palavra era: " + palavraSelecionada);
+        }
+
+        scanner.close();
     }
 }
-
